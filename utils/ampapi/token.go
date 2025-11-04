0b5f3561e25e036aa423c877c0193203c7b14361
Daylighttg <jeremihthesecond@gmail.com>
2025-11-04 11:11:12 +0800

    fix: update regex pattern for legacy index JS file matching to support additional delimiters

--- utils/ampapi/token.go
+++ utils/ampapi/token.go
@@ -23,7 +23,7 @@ func GetToken() (string, error) {
 		return "", err
 	}
 
-	regex := regexp.MustCompile(`/assets/index-legacy~[^/]+\.js`)
+	regex := regexp.MustCompile(`/assets/index-legacy[-~][^/]+\.js`)
 	indexJsUri := regex.FindString(string(body))
 
 	req, err = http.NewRequest("GET", "https://beta.music.apple.com"+indexJsUri, nil)
